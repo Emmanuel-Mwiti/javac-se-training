713b56138352293f82364fbf310547cba4a57dc1
User <kiuguemmanu@gmail.com>
2023-08-29 12:18:14 +0300

    Working with arrays first step

--- src/main/java/com/systechafrica/arrays/WorkingWithArrays.java
+++ src/main/java/com/systechafrica/arrays/WorkingWithArrays.java
@@ -1,5 +1,25 @@
 package com.systechafrica.arrays;
 
+import java.util.logging.Logger;
+
 public class WorkingWithArrays {
-  
+    private static final Logger LOGGER = Logger.getLogger(WorkingWithArrays.class.getName());
+    public void singleDimensionalArray() {
+        int[] numbers = {45, 10, 26, 35, 40, 59};
+        String[] nicknames = {"Mhusika", "Ntate", "Yokana"};
+        for (int number : numbers) {
+            LOGGER.info("" + number);
+        }
+        LOGGER.info( nicknames[0]);
+        LOGGER.info(nicknames[1]);
+        LOGGER.info( nicknames[2]);
+
+    }
+    public static void main(String [] args){
+        WorkingWithArrays app = new WorkingWithArrays();
+        //app.singleDimensionalArray();
+
+
+
+    }
 }
