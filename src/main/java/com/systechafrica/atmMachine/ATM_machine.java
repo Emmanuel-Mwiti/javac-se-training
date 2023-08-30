dc90c892e1e64aa6de008825f4d353e1ca2508c3
User <kiuguemmanu@gmail.com>
2023-08-30 11:51:04 +0300

    Added return statements for quit and if, and some few code cleaning

--- src/main/java/com/systechafrica/atmMachine/ATM_machine.java
+++ src/main/java/com/systechafrica/atmMachine/ATM_machine.java
@@ -9,17 +9,25 @@ public class ATM_machine {
     //  **HERE IS THE APPROACH STEP BY STEP I APPROACHED THE PROBLEM  **
     //  After Each solution, i commit the changes, therefore even the commit messages can show my approach. Approached the problem dividing it into sprints.
 
-    //  1.  First setting up how the first login, assuming the name is Emmanuel and default password. If this is is true, directed to the login page
-    //  2. Checking the number of login attempts to make sure they are not more than three. Here, i introduced a variable called loginTrial, and encopass the test login, and increment the login up to three. Also introduce a while loop to check login trials to be 3 after which we exist
-    //  3. Step 3 is now to get a way to display to user login failed after the three attempts, or else and incase even after 2nd attempt is successiful, we get to the welcome page
-    //  4. Next sprint now is to work with the users who have successifully logged in
-            // i.) Checking Balance- define it default to 1000, balance can have cents hence define instance variable balance to double and initialize it to 1000
-            // ii.) Deposits
-            // iii.) Withdraw
-            // iv.) transfer and print receipt
-  // While working here, i realized i should have globally created scanner object, small modifications.
-  // The use switch control to define the above ATM menu.
-  //  5. Now after all the logistics, the system should not be existing after each option, it should be taking us to the main menu.So, i will encompass the whole ATM menu under true evaluation such that it evaluates true. Also, it should reload the main option, therefore, i will remove it from the place of checking user and password to the first welcome message. Few Modifications also to only print login successiful once.
+    // 1.  First setting up how the first login, assuming the name is Emmanuel and default password. If this is is true, directed to the login page
+    // 2. Checking the number of login attempts to make sure they are not more than three. Here, i introduced a variable called loginTrial, and encopass the test login, and increment the login up to three. Also introduce a while loop to check login trials to be 3 after which we exist
+    // 3. Step 3 is now to get a way to display to user login failed after the three attempts, or else and incase even after 2nd attempt is successiful, we get to the welcome page
+    // 4. Next sprint now is to work with the users who have successifully logged in
+    //         i.) Checking Balance- define it default to 1000, balance can have cents hence define instance variable balance to double and initialize it to 1000
+    //         ii.) Deposits
+    //        iii.) Withdraw
+    //         iv.) transfer and print receipt
+  
+    //         While working here, i realized i should have globally created scanner object, small modifications.
+    //         The use switch control to define the above ATM menu.
+    // 5. Now after all the logistics, the system should not be existing after each option, it should be taking us 
+    //    to the main menu.
+    //    So, i will encompass the whole ATM menu under true evaluation such that it evaluates true. Also, 
+    //    it should reload the main option, therefore, i will remove it from the place of checking user 
+    //    and password to the first welcome message. Few Modifications also to only print login successiful once.
+    // 6. Enable the quite to work by adding a System.quit or return in case 5. Als0, to prevent the if 
+          //  checking the logins to continue execution, you exist from the whole function using return 
+          //  or system.ext
 
 
     final String USER_NAME = "Emmanuel";
@@ -38,8 +46,10 @@ public class ATM_machine {
       String username = scanner.nextLine();
       System.out.print("Password:");
       String userPassword = scanner.nextLine();
+
       //check if the password and user name are equal to what is provided
       if (username.equals(USER_NAME) && userPassword.equals(USER_PASSOWRD)) {
+        
         System.out.println("Login successiful");
         break; //when return true, it exists the control and goes to the next execution
       } else {
@@ -49,6 +59,7 @@ public class ATM_machine {
     }
     if (loginTrial == 3) {
       System.err.println("You have exhausted your login attempts, the system will now stop!");
+      return;
     }
 
     while (true) {
@@ -98,8 +109,7 @@ public class ATM_machine {
         break;
       case 4:
       case 5:
-      System.exit(0);
-        break;
+      return;
       default:
         System.out.print("Invalid option. Please enter a valid option! ");
         break;
