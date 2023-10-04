0c1c50eb8960698f76e668890a282b313f773f28
Emmanuel-Mwiti <kiuguemmanu@gmail.com>
2023-10-04 16:04:36 +0300

    updates

--- src/main/java/com/systechafrica/part4/utildate/funtionalprogramming/ArrayEvenNumbers.java
+++ src/main/java/com/systechafrica/part4/utildate/funtionalprogramming/ArrayEvenNumbers.java
@@ -2,10 +2,8 @@ package com.systechafrica.part4.utildate.funtionalprogramming;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ArrayEvenNumbers {
   public static void main(String[] args) {
@@ -16,6 +14,7 @@ public class ArrayEvenNumbers {
         .map(x -> x * x)
         .collect(Collectors.toList());
     System.out.println(evenAndSquared);
+
   }
 
   public static List<Integer> evenNumbers(List<Integer> numbers, Predicate<Integer> condition) {
---
+++ src/main/java/com/systechafrica/part4/utildate/funtionalprogramming/FunctionalProgrammingDemo.java
@@ -0,0 +1,7 @@
+package com.systechafrica.part4.utildate.funtionalprogramming;
+
+public class FunctionalProgrammingDemo {
+  public static void main(String[] args) {
+
+  }
+}
